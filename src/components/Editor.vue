b35274eda0376848785c6b7b74e163e3bce61ab6
zhangyunfei <5303221@gmail.com>
2020-03-20 21:50:42 +0800

    添加同步滚动

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -26,18 +26,23 @@
                 }"
                 @input="update"
                 @drop="drop($event)"
+                @scroll="onScrollEditor"
+                ref="editorBox"
                 @dragenter="dragenter($event)"
                 @dragover="dragover($event)">
       </textarea>
             <div id="editor_preview"
                  class="markdown-body"
                  v-html="compiledMarkdown()"
+                 @scroll="onScrollPreview"
+                 ref="preview"
                  :class="{
               'edit': edit_mode === 'edit',
               'preview': edit_mode === 'preview'
             }">
             </div>
-            <textarea name="content_html" :value="content_html" style="display: none"></textarea>
+            <textarea name="content_html" :value="content_html"
+                      style="display: none"></textarea>
         </div>
     </section>
 </template>
@@ -57,7 +62,8 @@
         window.Promise = Promise
     }
     //  import qs from 'qs'
-
+    let mainFlag = false; // 抵消两个滚动事件之间互相触发
+    let preFlag = false; // 如果两个 flag 都为 true，证明是反弹过来的事件引起的
     export default {
         props: ['markdown', 'upload_action_editor'],
         name: 'editor',
@@ -272,7 +278,40 @@
                 el.stopPropagation()
                 el.preventDefault()
                 this.fileList(el.dataTransfer.files)
-            }
+            },
+            scrollLink: function (event, who, editor, preview) {
+                let radio = (editor.scrollHeight - editor.clientHeight) / (preview.scrollHeight - preview.clientHeight);
+                if (who == 'preview') {
+                    preFlag = true;
+                    if (mainFlag === true) { // 抵消两个滚动事件之间互相触发
+                        mainFlag = false;
+                        preFlag = false;
+                        return;
+                    }
+                    // console.log(who,radio,preview.scrollHeight,editor.scrollHeight);
+                    editor.scrollTop = Math.round(preview.scrollTop * radio);
+                    return;
+                }
+                if (who == 'editor') {
+                    mainFlag = true;
+                    if (preFlag === true) { // 抵消两个滚动事件之间互相触发
+                        mainFlag = false;
+                        preFlag = false;
+                        return;
+                    }
+                    // console.log(who,radio);
+                    preview.scrollTop = Math.round(editor.scrollTop / radio);
+                    return;
+                }
+            },
+            //滚动编辑器事件
+            onScrollEditor(event) {
+                this.scrollLink(event, 'editor', this.$refs.editorBox, this.$refs.preview);
+            },
+            //滚动预览事件
+            onScrollPreview() {
+                this.scrollLink(event, 'preview', this.$refs.editorBox, this.$refs.preview);
+            },
         }
     }
 </script>
